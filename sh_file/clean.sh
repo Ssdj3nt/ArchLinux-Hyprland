8dfb7906bc4d02d41f6f7ff5d0b8045c25addc19
Francesco Porritiello <92294163+Ssdj3nt@users.noreply.github.com>
2025-02-19 19:49:46 +0100

    Add files via upload

--- sh_file/clean.sh
+++ sh_file/clean.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
+sudo bash -c 'pacman -Scc; yay -Scc; pacman -Qdtq | pacman -Rns -; pacman -Qqd | pacman -Rsu -'
 
-pacman -Scc
-yay -Scc
-pacman -Qdtq | pacman -Rns -
-pacman -Qqd | pacman -Rsu  -
